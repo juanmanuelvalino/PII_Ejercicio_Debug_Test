04d53012fd3fe03b2669b89c7cb8fe99d03237d8
Juan Manuel Valiño <juan.valino@mercadolibre.com>
2018-08-31 20:16:40 -0300

    Debug y Test

---
+++ src/Program/Program.cs
@@ -0,0 +1,31 @@
+﻿//-------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Universidad Católica del Uruguay">
+// Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//Alumno: Juan Manuel Valiño
+//e-Mail: jmvdm09@gmail.com
+//C.I.:5547360-8
+//-------------------------------------------------------------------------
+using System;
+
+/// <summary>
+/// Clase que contiene el punto de entrada al programa.
+/// </summary>
+namespace WordsPhrases
+{
+    public class Program
+    {
+        /// <summary>
+        /// El punto de entrada al programa.
+        /// </summary>
+        public static void Main()
+        {
+            Word hello = new Word("Hello");
+            Word world = new Word("World!");
+            Phrase greeting = new Phrase();
+            greeting.AddWord(hello);
+            greeting.AddWord(world);
+            Console.WriteLine(greeting.GetPhrase());
+        }
+    }
+}
